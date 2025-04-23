78467d7d17ee260aa4c5f5666ab8dd4f21dd40b1
Tilly Brooks <tildenbrooks24@gmail.com>
2025-04-23 16:05:23 -0700

    forgot to move the stimuli too

---
+++ stims.js
@@ -0,0 +1,23 @@
+// stimuli.js
+const experimentStimuli = [
+      {
+        sentence: ["John", "was", "taking", "a", "university", "course", "and", "working", "at", "the", "same", "time.", "For", "the", "exams", "he", "had", "to", "study", "from", "short", "and", "comprehensive", "sources.", "Depending", "on", "the", "course,", "he", "could", "read", "the", "class", "notes", "or", "the", "summary."],
+        condition: "Upper Bound"
+      },
+      {
+        sentence: ["John", "heard", "that", "the", "textbook", "for", "Geophysics", "was", "very", "advanced.", "Nobody", "understood", "it", "properly.", "He", "heard", "that", "if", "he", "wanted", "to", "pass", "the", "course", "he", "should", "read", "the", "class", "notes", "or", "the", "summary."],
+        condition: "Lower Bound"
+      },
+      {
+        sentence: ["The", "day's", "offer", "usually", "is:", "You", "can", "have", "a", "full", "menu", "for", "one", "person", "and", "the", "second", "person", "can", "have", "the", "plat", "de", "jour", "for", "free.", "Today,", "customers", "could", "have", "for", "free", "meat", "or", "fish."],
+        condition: "Upper Bound"
+      },
+      {
+        sentence: ["The", "dietician", "that", "visited", "the", "school", "explained", "to", "children", "how", "useful", "for", "our", "body", "protein", "can", "be.", "He", "also", "told", "them", "that", "we", "can", "find", "protein", "in", "meat", "or", "fish."],
+        condition: "Lower Bound"
+      },
+      {
+        sentence: ["Mary", "was", "saying", "that,", "John", "is", "so", "careless,", "that", "he", "constantly", "loses", "things.", "He", "has", "lost", "money,", "keys,", "credit", "cards.", "Once", "along", "with", "his", "bag,", "he", "even", "managed", "to", "loose", "his", "ID", "or", "his", "passport."],
+        condition: "Upper Bound"
+      }
+    ]
